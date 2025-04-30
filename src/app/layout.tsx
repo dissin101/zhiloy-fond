61c1dcce30abec1c8155f517c0c66e1a4bb74a73
Kirill Bushmanov <53367565+dissin101@users.noreply.github.com>
2025-04-30 19:05:53 +0500

    Set default fonts & color variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lora, Cormorant_SC } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lora = Lora({
+  variable: "--font-lora-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const cormorant = Cormorant_SC({
+  variable: "--font-cormorant-sc",
+    subsets: ["latin"],
+    weight: "700"
 });
 
 export const metadata: Metadata = {
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lora.variable} ${cormorant.variable} antialiased`}
       >
         {children}
       </body>
