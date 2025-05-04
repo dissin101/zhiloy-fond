a7bc275561b9463cc3ca1ddcacf15b470cba1ad2
Kirill Bushmanov <53367565+dissin101@users.noreply.github.com>
2025-05-04 16:44:04 +0500

    Change next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    output: 'export',
+    basePath: '/zhiloy-fond',
+    mages: {
+        unoptimized: true,
+    },
+}
 
-export default nextConfig;
+export default nextConfig
