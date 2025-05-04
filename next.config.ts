996054af5bd68252c841df19e2c230697436bc41
Kirill Bushmanov <53367565+dissin101@users.noreply.github.com>
2025-05-04 16:59:12 +0500

    Fix images config again

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
     output: 'export',
     basePath: '/zhiloy-fond',
+    assetPrefix: '/zhiloy-fond',
     images: {
         unoptimized: true,
-        path: '/zhiloy-fond',
     },
 }
 
