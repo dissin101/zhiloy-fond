a62bcd999d65df5c76435004d9dbcddcbfff33ec
Kirill Bushmanov <53367565+dissin101@users.noreply.github.com>
2025-05-06 22:16:58 +0500

    Add hover on primary button

--- src/app/components/ui/PrimaryButton.tsx
+++ src/app/components/ui/PrimaryButton.tsx
@@ -6,7 +6,7 @@ const PrimaryButton = (
     return (
         <button
             {...props}
-            className={`${props.className ?? ''} cursor-pointer bg-primary text-on-primary rounded-[100px] py-2.5 px-7.5 font-bold min-w-[200px] min-h-[44px]`}
+            className={`${props.className ?? ''} group cursor-pointer transition duration-300 ease-in-out bg-primary hover:bg-on-background text-on-primary hover:fill-primary hover:text-primary rounded-[100px] py-2.5 px-7.5 font-bold min-w-[200px] min-h-[44px]`}
         />
     )
 }
