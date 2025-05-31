69afd56bb8b9adf6ce02691cce834066c4f1a7ea
Kirill Bushmanov <53367565+dissin101@users.noreply.github.com>
2025-05-31 16:38:16 +0500

    Fix basePath

--- src/app/utils/image.ts
+++ src/app/utils/image.ts
@@ -1,4 +1,7 @@
 export function getImagePath(path: string): string {
-    const basePath = process.env.NODE_ENV === 'production' ? '/zhiloy-fond' : ''
+    //const basePath = process.env.NODE_ENV === 'production' ? '/zhiloy-fond' : ''
+
+    const basePath = ''
+
     return `${basePath}${path}`
 }
